bea805ac01d47bdd2b88730453b3daa2b8d8bbc5
Sabri <145472189+S4brinBR7@users.noreply.github.com>
2023-10-19 09:30:20 -0300

    Update sktech.js

--- sktech.js
+++ sktech.js
@@ -1,16 +1,15 @@
 function setup() {
-    createCanvas(600, 600);
-    background("black");
-  }
-  
-  function draw() {
-    stroke("rgb(176,0,255)");
-    fill("black");
-  
-    // console.log(mouseIsPressed)
-  
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 20, 35);
-    }
-  }
+  createCanvas(600, 600);
+  background("black");
+}
 
+function draw() {
+  stroke("rgb(176,0,255)");
+  fill("black");
+    
+  // console.log(mouseIsPressed)
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
